1e20c648213aed62497abea60bcad5210825978a
jespeer <jespeer@email.sc.edu>
2022-12-05 11:03:02 -0500

    changing gcd function

--- prime.go
+++ prime.go
@@ -24,50 +24,11 @@ func aks(n int) string {
 
 
 
-// Given two integers a and b, GCD(a,b) returns g,u,v where 
-// g is the gcd(a,b) and au+bv = g 
-func GCD(a, b int) (int,int,int) {
-	// if b = 0, then the gcd is a 
-	if b == 0 {
-		return a,1,0
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
 	}
-
-	// Keeps tracks of the sign of a and b and makes sure 
-	// a and b are non-negative 
-	var na , nb   bool 
-	if a < 0 {
-		na = true 
-		a *= -1 
-	}
-	if b < 0 {
-		nb = true
-		b *= -1 
-	}
-
-	// Variables we will return 
-	var g, u, v int 
-	u = 1 
-	g = a
-	x := 0  // keeps track of the number a's used in the Euclidean algorithm 
-	y := b  // keeps track of the denominator in the Euclidean algorithm
-	for y != 0 {
-		t := ModN(uint(y),g) // find t,q with g = qy + t 
-		q := g / y 
-		s := u - q*x  
-		u = x 
-		g = y 
-		x = s 
-		y = t 
-	}
-	v = (g-a*u)/b  
-	
-	if !na && !nb {
-		return g, u, v 
-	} else if !na && nb {
-		return g, u, -v 
-	} else if na && !nb {
-		return g, -u, v 
-	} else {
-		return g , -u, -v 
-	}
+	return a
 }
